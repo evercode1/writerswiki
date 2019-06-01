e05765062c0fb1f449332106fd859b9be3db6698
billk <ikon321@yahoo.com>
2019-05-31 22:17:14 -0700

    links

--- resources/views/layouts/home-partials/side-nav.blade.php
+++ resources/views/layouts/home-partials/side-nav.blade.php
@@ -6,9 +6,9 @@
                 <div class="collapsible-body">
                     <ul>
                         <li><a href="/home" class="waves-effect active">Dashboard<i class="material-icons">web</i></a></li>
-                        <li><a href="/all-stars" class="waves-effect">Emotions<i class="material-icons">star</i></a></li>
-                        <li><a href="/all-planets" class="waves-effect">Action Beats<i class="material-icons">star</i></a></li>
-                        <li><a href="/all-planets" class="waves-effect">Descriptions<i class="material-icons">star</i></a></li>
+                        <li><a href="#" class="waves-effect">Emotions<i class="material-icons">star</i></a></li>
+                        <li><a href="#" class="waves-effect">Action Beats<i class="material-icons">star</i></a></li>
+                        <li><a href="#" class="waves-effect">Descriptions<i class="material-icons">star</i></a></li>
 
                     </ul>
                 </div>
@@ -17,21 +17,21 @@
             <li class="bold waves-effect"><a class="collapsible-header">Videos<i class="material-icons chevron">chevron_left</i></a>
                 <div class="collapsible-body">
                     <ul>
-                        <li><a href="/all-videos" class="waves-effect">All Videos<i class="material-icons">videocam</i></a></li>
+                        <li><a href="#" class="waves-effect">All Videos<i class="material-icons">videocam</i></a></li>
                     </ul>
                 </div>
             </li>
             <li class="bold waves-effect"><a class="collapsible-header">Articles<i class="material-icons chevron">chevron_left</i></a>
                 <div class="collapsible-body">
                     <ul>
-                        <li><a href="/all-articles" class="waves-effect">All Articles<i class="material-icons">collections</i></a></li>
+                        <li><a href="#" class="waves-effect">All Articles<i class="material-icons">collections</i></a></li>
                     </ul>
                 </div>
             </li>
             <li class="bold waves-effect"><a class="collapsible-header">Books<i class="material-icons chevron">chevron_left</i></a>
                 <div class="collapsible-body">
                     <ul>
-                        <li><a href="/all-books" class="waves-effect">All Books<i class="material-icons">book</i></a></li>
+                        <li><a href="#" class="waves-effect">All Books<i class="material-icons">book</i></a></li>
                     </ul>
                 </div>
             </li>
