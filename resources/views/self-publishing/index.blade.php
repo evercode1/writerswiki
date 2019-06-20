993281ed0c9d1b9d9f4c59e1120d46c090b7bf99
billk <ikon321@yahoo.com>
2019-06-19 22:41:01 -0700

    self-publishing

--- resources/views/self-publishing/index.blade.php
+++ resources/views/self-publishing/index.blade.php
@@ -13,6 +13,19 @@
 
         <self-publishing></self-publishing>
 
+        @if(Auth::check() && Auth::user()->isContributor())
+
+            <div class="row">
+
+
+                <a href="/media-link/create" class="waves-effect waves-light btn">Create New</a>
+
+
+
+            </div>
+
+        @endif
+
 
     </div>
 
