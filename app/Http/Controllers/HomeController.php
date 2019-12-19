4dc2b457c85ea0d924e7b3fcd5b58d8b718437b5
billk <ikon321@yahoo.com>
2019-12-18 16:15:20 -0800

    video query limits to max vonne on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\MediaLink;
+use App\Profile;
 
 
 class HomeController extends Controller
@@ -13,6 +14,8 @@ class HomeController extends Controller
     {
 
         $video = $this->video();
+
+
 
         return view('home.index', compact('video'));
 
@@ -26,7 +29,10 @@ class HomeController extends Controller
     {
         $typeId = DB::table('media_link_types')->where('name', 'Videos')->value('id');
 
-        $videos = MediaLink::where('media_link_type_id', $typeId)->get();
+        $maxVonne = Profile::where('name', 'Max Vonne')->pluck('user_id');
+
+        $videos = MediaLink::where([['media_link_type_id', $typeId], ['user_id', $maxVonne], ['by_contributor', 1]])->get();
+
 
         $video = $videos->random(1);
 
