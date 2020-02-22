e85c51bc0ed0e6cf01a18e456e564780c2bb4d77
Bill Keck <billk@Bills-MacBook-Pro-2.local>
2020-02-22 15:09:48 -0800

    fixes search on channel list

--- app/Queries/ChannelLinksQuery.php
+++ app/Queries/ChannelLinksQuery.php
@@ -21,6 +21,12 @@ class ChannelLinksQuery
 
         $direction == 1 ? $direction = 'asc' : $direction = 'desc';
 
+        if ($keyword == null){
+
+            $keyword = $author;
+
+        }
+
 
         if($column == null){
 
@@ -49,6 +55,35 @@ class ChannelLinksQuery
 
         }
 
+        if($keyword == $author){
+
+
+            $rows = DB::table('media_links')
+                ->select('media_links.id as Id',
+                    'media_links.name as Name',
+                    'media_links.author as Author',
+                    'media_links.url as Url',
+                    'media_links.is_active as Active',
+                    'media_links.media_link_type_id as Type',
+                    'profiles.name as Contributor',
+                    'profiles.id as Profile',
+                    'categories.name as Category',
+                    'subcategories.name as Subcategory',
+                    DB::raw('DATE_FORMAT(media_links.created_at,
+                             "%m-%d-%Y") as Created'))
+                ->leftJoin('users', 'media_links.user_id', '=', 'users.id')
+                ->leftJoin('profiles', 'media_links.user_id', '=', 'profiles.user_id')
+                ->leftJoin('categories', 'category_id', '=', 'categories.id')
+                ->leftJoin('subcategories', 'subcategory_id', '=', 'subcategories.id')
+                ->where([['media_links.media_link_type_id', $type], ['media_links.author', $author ]])
+                ->orderBy($column, $direction)
+                ->paginate(10);
+
+            return $rows;
+
+
+        }
+
 
         $rows = DB::table('media_links')
             ->select('media_links.id as Id',
@@ -67,8 +102,7 @@ class ChannelLinksQuery
             ->leftJoin('profiles', 'media_links.user_id', '=', 'profiles.user_id')
             ->leftJoin('categories', 'category_id', '=', 'categories.id')
             ->leftJoin('subcategories', 'subcategory_id', '=', 'subcategories.id')
-            ->where([['media_links.media_link_type_id', $type], ['media_links.author', $author ]])
-            ->orWhere([['media_links.media_link_type_id', $type], ['media_links.author', $author ], ['media_links.name', 'like', '%' . $keyword . '%']])
+            ->where([['media_links.media_link_type_id', $type], ['media_links.author', $author ], ['media_links.name', 'like', '%' . $keyword . '%']])
             ->orWhere([['media_links.media_link_type_id', $type], ['media_links.author', $author ], ['categories.name', 'like', '%' . $keyword . '%']])
             ->orWhere([['media_links.media_link_type_id', $type], ['media_links.author', $author ], ['subcategories.name', 'like', '%' . $keyword . '%']])
             ->orderBy($column, $direction)
